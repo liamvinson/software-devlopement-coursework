5f6daaba6b5bd182f3b81ca54648cfdcf848f3f4
YazanHaddadin <yazanhaddadin7@gmail.com>
2017-10-31 17:44:14 +0000

    alot

---
+++ Bags.java
@@ -0,0 +1,21 @@
+package PebbleGame;
+import java.io.*;
+import java.util.*;
+
+public class Bags {
+	
+	private boolean isBlack;
+	private int[] pebbles = new int[100];
+	
+	public Bags(boolean isBlack) {
+		
+		this.isBlack = isBlack;
+		
+		if (isBlack == true){
+			//import list
+		}
+		else {
+			//empty list
+		}
+	}
+}
